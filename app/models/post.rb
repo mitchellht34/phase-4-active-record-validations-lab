732202447b49b9b2c6205389b981ebe242bf2c44
Mitch Toran <mitchellht34@gmail.com>
2022-06-10 23:09:22 -0400

    commit tree 9b032939c32c68e3e355365511bd4964dedb8d1c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :is_sufficient_clickbait
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def is_sufficient_clickbait
+        unless CLICKBAIT_PATTERNS.any?{ |pattern| pattern.match title }
+            errors.add(:title, "must be more clickbait-y")
+        end
+    end
 end
